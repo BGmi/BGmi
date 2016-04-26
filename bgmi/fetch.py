594e505e6490fa7924103c05db0d6092a35901fe
Ricter Z <ricterzheng@gmail.com>
2016-04-26 19:15:14 +0800

    setup.py

--- bgmi/fetch.py
+++ bgmi/fetch.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 from __future__ import print_function, unicode_literals
-import sys
 import re
 import datetime
 import string
--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
 # coding=utf-8
+from setuptools import setup, find_packages
+from bgmi import __version__, __author__, __email__
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='bgmi',
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    keywords='bangumi, bgmi, feed',
+    description='subscribe bangumi!',
+    url='https://github.com/RicterZ/BGmi',
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'bgmi = bgmi.main'
+        ]
+    },
+    license='MIT'
+)
