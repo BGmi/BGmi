7f45698be5ef5d6ecd305855fb9f5757bff37b76
Ricter Z <ricterzheng@gmail.com>
2016-05-06 11:14:39 +0800

    fix bug of xunlei-lixian

--- bgmi/download.py
+++ bgmi/download.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 from __future__ import print_function, unicode_literals
+import os
 import subprocess
 from bgmi.config import BGMI_LX_PATH, BGMI_SAVE_PATH
 from bgmi.models import Download
+from bgmi.utils import print_info
 
 
 def download_prepare(data):
@@ -12,9 +14,11 @@ def download_prepare(data):
 
 def download_xunlei_lixian(data):
     for i in data:
-        subprocess.call([BGMI_LX_PATH, 'download', '--torrent', '--overwrite',
-                         '--output-dir=%s' % BGMI_SAVE_PATH, i['download']],
-                         env={'PATH': '/usr/local/bin:/usr/bin:/bin'})
+        command = [BGMI_LX_PATH, 'download', '--torrent', '--overwrite',
+                   '--output-dir=%s' % BGMI_SAVE_PATH, i['download']]
+        print_info('Run command %s' % ' '.join(command))
+        subprocess.call(command, env={'PATH': '/usr/local/bin:/usr/bin:/bin',
+                                      'HOME': os.environ.get('HOME', '/tmp')})
         d = Download(**i)
         d.delete()
 
