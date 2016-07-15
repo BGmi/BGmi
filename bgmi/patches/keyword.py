6becc8ec576a4a0b678366cb6eaa5ad10962266d
Ricter Z <ricterzheng@gmail.com>
2016-07-15 20:56:16 +0800

    ☆ ☆ ☆ !

--- bgmi/patches/keyword.py
+++ bgmi/patches/keyword.py
@@ -4,6 +4,8 @@ from __future__ import print_function, unicode_literals
 patch_dict = {
     '魔法少女☆伊莉雅 3rei!!': 'Liner%7C莉雅%203re',
     '槍彈辯駁3': '論破3',
+    '槍彈辯駁3未來篇': '論破3%20未来',
+    '槍彈辯駁3絕望篇': '論破3%20绝望',
     '食戟之靈 貳之皿': '食戟%20皿',
 }
 
