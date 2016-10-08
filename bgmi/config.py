c051f2fd31de18b7eee8005b2e55596424b36179
Ricter Z <ricterzheng@gmail.com>
2016-10-08 15:33:45 +0800

    _(:3

--- bgmi/config.py
+++ bgmi/config.py
@@ -138,7 +138,7 @@ IS_PYTHON3 = sys.version_info > (3, 0)
 
 # Detail URL
 if os.environ.get('TRAVIS_CI', None):
-    FETCH_URL = 'http://bangumi.ricterz.me/calendars/2016-2.html'  # for test
+    FETCH_URL = 'http://bangumi.ricterz.me/calendars/2016-3.html'  # for test
 else:
     FETCH_URL = '{0}/cms/page/name/programme.html'.format(DMHY_URL)
 
