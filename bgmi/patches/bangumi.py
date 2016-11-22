051ea1d7d833c6e53137cf9e489f0ce82977581a
Ricter Z <ricterzheng@gmail.com>
2016-11-22 08:39:40 +0800

    new patches

--- bgmi/patches/bangumi.py
+++ bgmi/patches/bangumi.py
@@ -5,8 +5,12 @@ import inspect
 
 class Patch(object):
     def patch_bangumi_parser_netoge_no_yome_wa_onnanoko_ja_nai_to_omotta(self, data):
-        # patch for 线上游戏的老婆不可能是女生？
+        # patching for 线上游戏的老婆不可能是女生？
         return [i for i in data if '[TSDM&LoveEcho!漫画组]' not in i['title']]
+
+    def patch_www_working(self, data):
+        # patching for WWW.WORKING!!
+        return [i for i in data if '[WWW.迷糊餐厅!! WWW.WORKING!!][80][BIG5]' not in i['title']]
 
 
 def main(data):
