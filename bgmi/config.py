8e179b7deef189f02c4f4c27a765cf798db9dfdb
Ricter Z <ricterzheng@gmail.com>
2017-03-08 10:16:10 +0800

    use mirror

--- bgmi/config.py
+++ bgmi/config.py
@@ -105,7 +105,7 @@ def write_config(config=None, value=None):
 
 # --------- Writeable ---------- #
 # Setting dmhy url
-BANGUMI_MOE_URL = 'https://bangumi.moe'
+BANGUMI_MOE_URL = 'http://bangumi_moe.ricterz.me'
 
 # BGmi user path
 BGMI_SAVE_PATH = os.path.join(BGMI_PATH, 'bangumi')
