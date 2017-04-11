92509e3dbbe53878ad087525bb5dc6257b0bbc22
Ricter Z <ricterzheng@gmail.com>
2017-04-11 20:46:59 +0800

    remove tests

--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-import unittest
-
-
-class UtilsTest(unittest.TestCase):
-    def test_test_connection(self):
-        # Just for now..
-        # self.assertEqual(False, test_connection())
-        pass
