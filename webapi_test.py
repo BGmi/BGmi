f7fc4101d5406f310ea1e67858b277b939e96f31
Ricter Z <ricterzheng@gmail.com>
2017-10-04 19:17:44 +0800

    update tests

--- webapi_test.py
+++ webapi_test.py
@@ -23,15 +23,24 @@ api_list = [
             'name': os.environ.get('BANGUMI_2')
         }),
         'show_output': True
+    }, {
+        'action': 'update',
+        'method': 'get',
+        'show_output': True
     }
 ]
 
 
 if __name__ == '__main__':
+    exit_code = 0
     for api in api_list:
         r = getattr(requests, api['method'])('http://localhost:8888/api/{}'.format(api['action']),
                                              data=api.get('params', None),
                                              headers={'BGmi-Token': '233'}).json()
         print(api.get('params', None))
         if api['show_output']:
+            if r['status'] == 'error':
+                exit_code = 1
             print(r)
+
+    exit(exit_code)
