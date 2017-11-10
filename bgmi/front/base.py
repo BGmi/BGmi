51ecdaa96d438ff398ba23bf830005c5e36b145c
Trim21 <trim21me@gmail.com>
2017-11-11 04:15:03 +0800

    dev cros header

--- bgmi/front/base.py
+++ bgmi/front/base.py
@@ -1,6 +1,7 @@
 #!coding: utf-8
-import os
 import json
+import os
+
 import tornado.web
 
 from bgmi import __version__, __admin_version__
@@ -22,11 +23,7 @@ class BaseHandler(tornado.web.RequestHandler):
         self.finish()
 
     def options(self, *args, **kwargs):
-        self.add_header('Access-Control-Allow-Origin', 'http://localhost:8080')
-        self.add_header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
-        self.add_header("Access-Control-Allow-Headers",
-                        "Content-Type,bgmi-token,bgmi-token, "
-                        "Access-Control-Allow-Headers, Authorization, X-Requested-With")
+        pass
 
     def get(self, *args, **kwargs):
         self._method_not_allowed()
--- bgmi/front/server.py
+++ bgmi/front/server.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 from __future__ import print_function, unicode_literals
 
+import os
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -17,6 +19,15 @@ define('port', default=8888, help='listen on the port', type=int)
 define('address', default='0.0.0.0', help='binding at given address', type=str)
 
 API_ACTIONS = '%s|%s' % ('|'.join(API_MAP_GET.keys()), '|'.join(API_MAP_POST.keys()))
+
+if os.environ.get('DEV'):  # pragma: no cover
+    def prepare(self):
+        self.set_header('Access-Control-Allow-Origin', 'http://localhost:8080')
+        self.set_header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+        self.set_header("Access-Control-Allow-Headers", "Content-Type, bgmi-token, X-Requested-With")
+
+
+    tornado.web.RequestHandler.prepare = prepare
 
 
 def make_app(**kwargs):
