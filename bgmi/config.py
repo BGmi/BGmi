a58515d2247aed511ae02ec5c98bdb482946dbb4
Ricter Zheng <RicterZheng@gmail.com>
2019-01-10 16:23:46 +0800

    add global filter

--- bgmi/config.py
+++ bgmi/config.py
@@ -257,7 +257,7 @@ TRANSMISSION_RPC_PASSWORD = 'your_password'
 BANGUMI_TAG = '549ef207fe682f7549f1ea90'
 
 # Global blocked keyword
-GLOBAL_FILTER = 'Leopard-Raws, hevc, x265, c-a Raws'
+GLOBAL_FILTER = 'Leopard-Raws, hevc, x265, c-a Raws, U3-Web'
 
 # enable global filter
 ENABLE_GLOBAL_FILTER = '1'
