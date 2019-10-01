fa314abad05feaee979139dc530e14a8d6c9bfe3
Trim21 <i@trim21.me>
2019-10-01 14:46:30 +0800

    build: use poetry

--- .ci/install.bash
+++ .ci/install.bash
@@ -5,11 +5,11 @@ set -ex
 python --version
 pip --version
 
-pip install -r requirements/dev.txt
-pip install -q codecov
-pip install -e .
+curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
+export PATH=$HOME/.poetry/bin:$PATH
+poetry config settings.virtualenvs.create false
+poetry install -E mysql
 
 if [[ "$DB" == "mysql" ]]; then
   mysql -u root -e 'CREATE DATABASE IF NOT EXISTS bgmi DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;';
-  pip install pymysql;
 fi
