2cdb1cdf9482c27b9d0d718fe92c705d89cd74e5
Trim21 <i@trim21.me>
2019-10-16 22:26:12 +0800

    chore(ci): speed up installing deps

--- .ci/install.bash
+++ .ci/install.bash
@@ -7,6 +7,9 @@ pip --version
 
 curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 export PATH=$HOME/.poetry/bin:$PATH
+pip install toml==0.10.0
+python .circleci/export_requirements.py --all
+pip install --require-hashes --no-deps -r requirements.txt
 poetry config settings.virtualenvs.create false
 poetry install -E mysql
 
