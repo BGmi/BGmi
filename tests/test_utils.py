75a2356409865d2314af85244919d669c4ad9e40
Trim21 <i@trim21.me>
2020-09-11 02:06:01 +0800

    refactor(internal): migrate attrs to pydantic

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,12 +11,12 @@ def test_print_config():
 def test_remove_dupe():
     e = Episode.remove_duplicated_bangumi(
         [
-            Episode(name=1, title="1", download="1", episode=1),
-            Episode(name=1, title="1", download="1", episode=1),
-            Episode(name=2, title="2", download="2", episode=2),
-            Episode(name=2, title="2", download="2", episode=2),
-            Episode(name=3, title="3", download="3", episode=3),
-            Episode(name=5, title="5", download="5", episode=5),
+            Episode(name="1", title="1", download="1", episode=1),
+            Episode(name="1", title="1", download="1", episode=1),
+            Episode(name="2", title="2", download="2", episode=2),
+            Episode(name="2", title="2", download="2", episode=2),
+            Episode(name="3", title="3", download="3", episode=3),
+            Episode(name="5", title="5", download="5", episode=5),
         ]
     )
     assert len(e) == 4, e
@@ -26,14 +26,14 @@ def test_remove_dupe():
 def test_episode_regex():
     e = episode_filter_regex(
         [
-            Episode(name=1, title="720", download="1", episode=1),
-            Episode(name=2, title="1080", download="1", episode=1),
-            Episode(name=2, title="23", download="2", episode=2),
-            Episode(name=1, title="17202", download="2", episode=2),
-            Episode(name=3, title="..71..", download="3", episode=3),
-            Episode(name=5, title="no", download="5", episode=5),
+            Episode(name="1", title="720", download="1", episode=1),
+            Episode(name="2", title="1080", download="1", episode=1),
+            Episode(name="2", title="23", download="2", episode=2),
+            Episode(name="1", title="17202", download="2", episode=2),
+            Episode(name="3", title="..71..", download="3", episode=3),
+            Episode(name="5", title="no", download="5", episode=5),
         ],
         ".*720.*",
     )
     assert len(e) == 2, e
-    assert {x.name for x in e} == {1}
+    assert {x.name for x in e} == {"1"}
