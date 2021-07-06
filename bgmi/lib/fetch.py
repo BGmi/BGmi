3d46ef363476acceef92cfa295047bca71710493
Trim21 <i@trim21.me>
2021-07-06 22:36:52 +0800

    chore: update lock file

--- bgmi/lib/fetch.py
+++ bgmi/lib/fetch.py
@@ -4,11 +4,11 @@ from bgmi.config import DATA_SOURCE
 from bgmi.utils import print_error
 from bgmi.website import bangumi_moe, base, mikan, share_dmhy
 
-DATA_SOURCE_MAP = {
+DATA_SOURCE_MAP: Dict[str, Type[base.BaseWebsite]] = {
     "mikan_project": mikan.Mikanani,
     "bangumi_moe": bangumi_moe.BangumiMoe,
     "dmhy": share_dmhy.DmhySource,
-}  # type: Dict[str, Type[base.BaseWebsite]]
+}
 
 
 try:
