44bdc99c4beff1e24c20f330d01a84d524043bb6
Trim21 <trim21.me@gmail.com>
2021-07-10 00:50:10 +0800

    fix tests

--- tests/downloader/test_aria2.py
+++ tests/downloader/test_aria2.py
@@ -5,8 +5,9 @@ from bgmi.downloader.aria2_rpc import Aria2DownloadRPC
 _token = "token:2334"
 
 
-@mock.patch("bgmi.config.ARIA2_RPC_URL", "uuu")
+@mock.patch("bgmi.config.ARIA2_RPC_URL", "https://uuu")
 def test_use_config():
     with mock.patch("xmlrpc.client.ServerProxy") as m1:
+        m1.return_value.aria2.getVersion.return_value = {"version": "1.19.1"}
         Aria2DownloadRPC()
-        m1.assert_called_once_with("uuu")
+        m1.assert_called_once_with("https://uuu")
