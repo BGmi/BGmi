c89010eda325a71818cac5223ed960a3b543a2aa
Trim21 <trim21.me@gmail.com>
2021-07-10 01:14:33 +0800

    fix test

--- tests/downloader/test_aria2.py
+++ tests/downloader/test_aria2.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 from bgmi.downloader.aria2_rpc import Aria2DownloadRPC
+from bgmi.plugin.status import DownloadStatus
 
 _token = "token:2333"
 
@@ -10,9 +11,11 @@ def test_use_config():
     with mock.patch("xmlrpc.client.ServerProxy") as m1:
         m1.return_value.aria2.getVersion.return_value = {"version": "1.19.1"}
         Aria2DownloadRPC()
-        m1.assert_has_calls([mock.call("https://uuu")])
+        m1.assert_called_with("https://uuu")
 
 
 @mock.patch("bgmi.config.ARIA2_RPC_TOKEN", _token)
-def test_add_download(torrent_url: str):
-    Aria2DownloadRPC().add_download(url=torrent_url, save_path="/downloads/")
+def test_workflow(torrent_url: str):
+    rpc = Aria2DownloadRPC()
+    info_hash = rpc.add_download(url=torrent_url, save_path="/downloads/")
+    assert rpc.get_status(info_hash) == DownloadStatus.downloading
