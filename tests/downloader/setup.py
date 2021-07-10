684ed94ab00f0bb9a4ff56ce566dd02b384318c9
Trim21 <trim21.me@gmail.com>
2021-07-10 13:31:24 +0800

    add comments

--- tests/downloader/setup.py
+++ tests/downloader/setup.py
@@ -2,6 +2,10 @@ from bgmi.downloader import DelugeRPC
 
 client = DelugeRPC()
 
+# deluge的Web需要进行初始化
+# 但是用户的deluge应该已经初始化过了
+# 只需要在测试环境中进行
+
 if not client._call("web.connected"):
     connected_web = client._call("web.get_hosts")
     host = connected_web[0][1]
