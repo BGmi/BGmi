97bd16c29aec15d65b117b096adde270cf7a48fd
Trim21 <trim21.me@gmail.com>
2023-03-26 14:50:18 +0800

    fix test

--- bgmi/session.py
+++ bgmi/session.py
@@ -5,14 +5,14 @@ import pickle
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
-from bgmi.config import TMP_PATH
+from bgmi.config import cfg
 
 session = requests.Session()
 
 retries = Retry(total=3, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
 session.mount("https://mikanani.me/", HTTPAdapter(max_retries=retries))
 
-cookies_file = pathlib.Path(TMP_PATH).joinpath("mikan_cookies.txt")
+cookies_file = pathlib.Path(cfg.tmp_path).joinpath("mikan_cookies.txt")
 
 if cookies_file.exists():
     with cookies_file.open("rb") as f:
