b6439be6bdc9e8f44b1b108a51b4831e302e4a7b
Trim21 <trim21.me@gmail.com>
2023-04-06 00:36:48 +0800

    refactor: unify column name

--- bgmi/lib/download.py
+++ bgmi/lib/download.py
@@ -29,8 +29,7 @@ def download_prepare(data: List[Episode]) -> None:
     queue = save_to_bangumi_download_queue(data)
     driver = get_download_driver(cfg.download_delegate)
     for download in queue:
-        download.created_time = int(time.time())
-        save_path = bangumi_save_path(download.name).joinpath(str(download.episode))
+        save_path = bangumi_save_path(download.bangumi_name).joinpath(str(download.episode))
         if not save_path.exists():
             os.makedirs(save_path)
 
