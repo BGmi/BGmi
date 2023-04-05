957200d03c5bee5216300594f3a68a733600212a
Trim21 <trim21.me@gmail.com>
2023-04-06 00:09:16 +0800

    fix: python3.9 syntax

--- bgmi/lib/table.py
+++ bgmi/lib/table.py
@@ -126,7 +126,7 @@ class Bangumi(Base):
         weekly_list = defaultdict(list)
         for bangumi_item, followed_status, episode in data:
             weekly_list[bangumi_item.update_time.lower()].append(
-                bangumi_item.__dict__ | {"status": followed_status, "episode": episode}
+                {**bangumi_item.__dict__, "status": followed_status, "episode": episode}
             )
 
         return weekly_list
