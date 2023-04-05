5b060c68e123507cfa237fb2c69fce64083a4a53
Trim21 <trim21.me@gmail.com>
2023-04-05 16:25:00 +0800

    fix: cookies dump/loading

--- bgmi/session.py
+++ bgmi/session.py
@@ -18,17 +18,16 @@ session.mount("https://mikanani.me/", HTTPAdapter(max_retries=retries))
 cookies_file = pathlib.Path(cfg.tmp_path).joinpath("mikan_cookies.txt")
 
 if cookies_file.exists():
-    with cookies_file.open("rb") as f:
-        data = {}
-        try:
-            data = pickle.load(f)
-        except pickle.UnpicklingError:
-            pass
-        session.cookies.update(data)
+    data = {}
+    dump = cookies_file.read_bytes()
+    try:
+        data = pickle.loads(dump)
+    except pickle.UnpicklingError:
+        cookies_file.unlink()
+    session.cookies.update(data)
 
 
 @atexit.register
 def save_cookies() -> None:
     if cookies_file.parent.exists() and cookies_file.parent.is_dir():
-        with open(cookies_file, "wb") as f:
-            pickle.dump(session.cookies, f)
+        cookies_file.write_bytes(pickle.dumps(session.cookies))
