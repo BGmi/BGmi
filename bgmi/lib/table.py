1ab4565f282532b4fa6ff58e58fee65142257334
Trim21 <trim21.me@gmail.com>
2023-04-05 20:30:04 +0800

    fix

--- bgmi/lib/table.py
+++ bgmi/lib/table.py
@@ -28,7 +28,7 @@ STATUS_DOWNLOADING = 1
 STATUS_DOWNLOADED = 2
 DOWNLOAD_STATUS = (STATUS_NOT_DOWNLOAD, STATUS_DOWNLOADING, STATUS_DOWNLOADED)
 
-engine = create_engine(f"sqlite:///{cfg.db_path.as_posix()}")
+engine = create_engine(f"sqlite:///{cfg.db_path.absolute().as_posix()}")
 # if os.getenv("DEV")
 # engine = create_engine("sqlite:///./tmp/bangumi.db")
 
