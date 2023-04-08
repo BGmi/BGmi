5bb7e8bd12453f08545c71064eaf7dc7599eadc1
Trim21 <trim21.me@gmail.com>
2023-04-09 02:05:00 +0800

    add noop downloader

--- bgmi/downloader/__init__.py
+++ bgmi/downloader/__init__.py
@@ -1,6 +1,7 @@
 from .aria2_rpc import Aria2DownloadRPC
 from .deluge import DelugeRPC
+from .noop import Noop
 from .qbittorrent import QBittorrentWebAPI
 from .transmission import TransmissionRPC
 
-__all__ = ["Aria2DownloadRPC", "DelugeRPC", "QBittorrentWebAPI", "TransmissionRPC"]
+__all__ = ["Aria2DownloadRPC", "DelugeRPC", "QBittorrentWebAPI", "TransmissionRPC", "Noop"]
