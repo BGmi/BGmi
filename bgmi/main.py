69f8f03118f33429c021d15eb737fe51e418a814
Trim21 <trim21.me@gmail.com>
2023-05-24 08:15:24 +0800

    feat: command `bgmi seed forget`

--- bgmi/main.py
+++ bgmi/main.py
@@ -638,8 +638,18 @@ def debug_info() -> None:
     print(f"arch: `{platform.architecture()}`")
 
 
-@cli.command("completion")
-@click.argument("shell", required=True)
-def completion(shell: str) -> None:
-    completer = Completer(cli)
-    print(completer.render(shell))
+@cli.group("seed")
+def seen() -> None:
+    ...
+
+
+@seen.command("forget")
+@click.argument("name", required=True)
+@click.argument("episode", required=True, type=int)
+def seen_forget(name: str, episode: int) -> None:
+    e = Followed.get(Followed.bangumi_name == name)
+    if episode not in e.episodes:
+        print_error("failed to remove episode")
+        return
+    e.episodes.remove(episode)
+    e.save()
