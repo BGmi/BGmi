8e81c0821fcf1276d3eff792dd74017145072fc9
Trim21 <trim21.me@gmail.com>
2023-06-03 08:12:41 +0800

    refactor: mypy cross platform

--- bgmi/utils/__init__.py
+++ bgmi/utils/__init__.py
@@ -4,8 +4,7 @@ import itertools
 import json
 import os
 import re
-import struct
-import subprocess
+import shutil
 import sys
 import tarfile
 import time
@@ -103,58 +102,9 @@ def bug_report() -> None:  # pragma: no cover
     )
 
 
-_DEFAULT_TERMINAL_WIDTH = 80
-
-
 def get_terminal_col() -> int:  # pragma: no cover
-    # pylint: disable=import-outside-toplevel,import-error
-    # https://gist.github.com/jtriley/1108174
-    if sys.platform == "win32":
-        try:
-            from ctypes import create_string_buffer, windll  # type: ignore[attr-defined]
-
-            # stdin handle is -10
-            # stdout handle is -11
-            # stderr handle is -12
-            h = windll.kernel32.GetStdHandle(-12)
-            csbi = create_string_buffer(22)
-            res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
-            if res:
-                (
-                    bufx,
-                    bufy,
-                    curx,
-                    cury,
-                    wattr,
-                    left,
-                    top,
-                    right,
-                    bottom,
-                    maxx,
-                    maxy,
-                ) = struct.unpack("hhhhHhhhhhh", csbi.raw)
-                sizex = right - left + 1  # type: int
-                return sizex
-
-            cols = int(subprocess.check_output("tput cols"))
-            return cols
-        except Exception:
-            return _DEFAULT_TERMINAL_WIDTH
-    else:
-        import fcntl
-        import termios
-
-        try:
-            col = struct.unpack(
-                "HHHH",
-                fcntl.ioctl(0, termios.TIOCGWINSZ, struct.pack("HHHH", 0, 0, 0, 0)),
-            )[
-                1
-            ]  # type: int
-
-            return col
-        except Exception:
-            return _DEFAULT_TERMINAL_WIDTH
+    w, h = shutil.get_terminal_size()
+    return w
 
 
 FRONTEND_NPM_URL = "https://registry.npmjs.com/bgmi-frontend/"
