9ade342ef2134e67a61186cd01d81e34640dfafe
Trim21 <trim21.me@gmail.com>
2023-07-08 13:45:03 +0800

    feat: add default trackers to magent url

--- bgmi/config.py
+++ bgmi/config.py
@@ -9,12 +9,16 @@ from typing import cast
 import typing
 
 import pydantic
-import strenum
 import tomlkit
 from pydantic import BaseModel, ConfigDict, Field, HttpUrl
 
+try:
+    from enum import StrEnum
+except ImportError:
+    from strenum import StrEnum
 
-class Source(strenum.StrEnum):
+
+class Source(StrEnum):
     Mikan = "mikan_project"
     BangumiMoe = "bangumi_moe"
     Dmhy = "dmhy"
